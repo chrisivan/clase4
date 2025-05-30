4243f514dc223f0822d262736054b664dc4b2247
Christian Schneider <chrisivan007@hotmail.com>
2025-05-30 11:06:18 -0300

    muestro la edad

--- MiConsolaDemo/Program.cs
+++ MiConsolaDemo/Program.cs
@@ -3,3 +3,7 @@
 Console.WriteLine("Ingrese su nombre:");
 var nombre = Console.ReadLine();
 Console.WriteLine("Bienvenido " + nombre);
+
+Console.WriteLine("Ingrese tu edad:");
+var edad = Console.ReadLine();
+Console.WriteLine("Tu edad es " + edad);
