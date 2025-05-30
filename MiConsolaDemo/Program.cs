0f93e8aa9f82fdcfea7b9bad18714a28bf579106
Christian Schneider <chrisivan007@hotmail.com>
2025-05-30 10:45:07 -0300

    agrego comentario de funcionalidad

--- MiConsolaDemo/Program.cs
+++ MiConsolaDemo/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+//Esta es la pantalla de inicio de mi programa
 Console.WriteLine("Ingrese su nombre:");
 var nombre = Console.ReadLine();
 Console.WriteLine("Bienvenido " + nombre);
